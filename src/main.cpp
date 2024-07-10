c976d29d3a0db791944d67f7d4b3b0066b5217a7
Tyler Kowalski <tkowalski9938@gmail.com>
2024-07-10 15:30:17 -0400

    initial ppm

---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main() {
+    // image
+    int IMAGE_WIDTH = 256;
+    int IMAGE_HEIGHT = 256;
+
+    // render
+    std::cout << "P3\n" << IMAGE_WIDTH << ' ' << IMAGE_HEIGHT << "\n255\n";
+
+    for (int m = 0; m < IMAGE_HEIGHT; ++m) {
+        for (int n = 0; n < IMAGE_WIDTH; ++n ) {
+            auto r = double(n) / (IMAGE_WIDTH - 1);
+            auto g = double(m) / (IMAGE_HEIGHT - 1);
+            auto b = 0.0;
+
+            int ir = int(255.999 * r);
+            int ig = int(255.999 * g);
+            int ib = int(255.999 * b);
+
+            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+        }   
+    }
+} 
