56b6a9e4b2de939b15e895c7913a38f13cbdd653
Tyler Kowalski <tkowalski9938@gmail.com>
2024-07-15 20:19:38 -0400

    hollow sphere

--- src/main.cpp
+++ src/main.cpp
@@ -11,13 +11,17 @@ int main() {
   HittableList world;
 
   auto material_ground = make_shared<Lambertian>(Colour(0.8, 0.8, 0.0));
-  auto material_center = make_shared<Dialectric>(1.00 / 1.33); // air bubble
+  auto material_center = make_shared<Dialectric>(1.5);
+  auto material_bubble = make_shared<Dialectric>(1.00 / 1.50);
   auto material_left = make_shared<Metal>(Colour(0.5, 0.5, 0.5), 0.1);
   auto material_right = make_shared<Metal>(Colour(0.1, 0.1, 0.1), 1.0);
 
   world.add(
       make_shared<Sphere>(Point3(0.0, -100.5, -1.0), 100.0, material_ground));
+
   world.add(make_shared<Sphere>(Point3(0.0, 0.0, -1.2), 0.5, material_center));
+  world.add(make_shared<Sphere>(Point3(0, 0, -1.2), 0.4, material_bubble));
+
   world.add(make_shared<Sphere>(Point3(-1.0, 0.0, -1.0), 0.5, material_left));
   world.add(make_shared<Sphere>(Point3(3.0, 0.0, -5.0), 1.0, material_right));
 
